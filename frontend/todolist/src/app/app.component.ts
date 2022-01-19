25abc8e54e5ca4125a79244b6ac0562075027852
KaspArno <kasper.skjeggestad@gmail.com>
2022-01-19 22:09:41 +0100

    implementet tutorial code

--- frontend/todolist/src/app/app.component.ts
+++ frontend/todolist/src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todolist';
+
+  public items: string[] = []; // an empty array that is responsible to add a division
+  public newTask: string = ''; // A two-way binding preformed witch pushes text on division
+
+  // When input is empty, it will not cerate a new division
+  public addToList() {
+    if (this.newTask == '') {} else {
+      this.items.push(this.newTask);
+      this.newTask = '';
+    }
+  }
+
+  // This fuinction takes to input the task, that has to be deleted
+  public deleteTask(index: number) {
+    this.items.splice(index, 1);
+  }
+
+
+
 }
