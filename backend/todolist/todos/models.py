a05172e370ed2f300140c0dfdd3c9e008d699090
KaspArno <kasper.skjeggestad@gmail.com>
2022-01-22 23:38:33 +0100

    added ToDo model in django

--- backend/todolist/todos/models.py
+++ backend/todolist/todos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class ToDo(models.Model):
+    description = models.CharField(max_length=200)
+    done = models.BooleanField(default=False)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    due = models.DateTimeField(null=True)
